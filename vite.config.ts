ac64430afedc09a87ebb54dc07c54341eb9194db
jammeh1 <105488430+jammeh1@users.noreply.github.com>
2026-04-17 09:20:26 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ].filter(Boolean),
+export default defineConfig({
+  base: "/dewastrading/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
